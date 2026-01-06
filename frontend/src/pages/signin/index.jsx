f3f85435efc7fc7e660d6953a02cbc8439b9f359
kakii-git <ka.kii1422.thaumiel@gmail.com>
2026-01-06 18:47:13 +0900

    /signigと/signupを削除してフォルダ整理、folder_structure.txtを修正

--- frontend/src/pages/signin/index.jsx
+++
@@ -1,1 +0,0 @@
-export default function SigninPage() { return <div className='p-4'><h1>ログイン画面</h1></div>; }
--- frontend/src/pages/signup/index.jsx
+++
@@ -1,1 +0,0 @@
-export default function SignupPage() { return <div className='p-4'><h1>新規登録画面</h1></div>; }
